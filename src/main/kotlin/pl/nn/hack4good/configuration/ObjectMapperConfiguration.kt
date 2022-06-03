61adc0b03067736f198d18b98d18365409d3e8cd
Hubert Chylik <hubert.chylik@nn.pl>
2022-06-03 11:17:37 +0200

    Test endpoint

---
+++ src/main/kotlin/pl/nn/hack4good/configuration/ObjectMapperConfiguration.kt
@@ -0,0 +1,19 @@
+package pl.nn.hack4good.configuration
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ObjectMapperConfiguration {
+    @Bean
+    fun objectMapper(): ObjectMapper? {
+        return ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .registerModule(KotlinModule())
+            .registerModule(JavaTimeModule())
+    }
+}
